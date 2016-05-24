30e55f1fe4cdc086fc9f2869141b196b12b75cfd
maxbobchin <garrett.blackham@gmail.com>
2016-05-24 15:28:39 -0600

    rough layout
    adding a rough layout. This doesn't include any images and lots of dummy
    data. This should give us an idea for how to layout the site
    potentially. Url to view is localhost/blackham/pages/main.zul

---
+++ blackham/src/main/java/com/vigilant/blackham/MainViewModel.java
@@ -0,0 +1,34 @@
+package com.vigilant.blackham;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.GlobalCommand;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Executions;
+
+
+public class MainViewModel {
+
+	private int count;
+
+	@Init
+	public void init() {
+		count = 100;
+	}
+
+	@Command
+	@NotifyChange("count")
+	public void cmd() {
+		++count;
+	}
+
+	public int getCount() {
+		return count;
+	}
+	
+	@GlobalCommand
+	public void logout() {
+//		AuthenticationService.logout();
+		Executions.sendRedirect("/");
+	}
+}
