ecc132323e90378458a4547acdc57c5041eaea45
maxbobchin <garrett.blackham@gmail.com>
2016-06-03 13:07:51 -0600

    Adding spreadsheet to zk
    Adding the zk spreadsheet dependencies to pom. added the spreadsheet to
    page test1.zul page. other minor changes.

--- blackham/src/main/java/com/vigilant/blackham/MainViewModel.java
+++ blackham/src/main/java/com/vigilant/blackham/MainViewModel.java
@@ -1,19 +1,28 @@
 package com.vigilant.blackham;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.GlobalCommand;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.Executions;
+//import org.zkoss.zss.ui.Spreadsheet;
 
 
 public class MainViewModel {
-
-	private int count;
+	public static Logger logger = LoggerFactory.getLogger("blackham.vigilant");
+	private int count;	
+	
+	private String workspacePage = WORKSPACE;
+	private final static String WORKSPACE = "workspace.zul";
 
 	@Init
 	public void init() {
+		logger.trace("MainViewModel.init");
 		count = 100;
+//		Spreadsheet a = null;
 	}
 
 	@Command
@@ -26,6 +35,21 @@ public class MainViewModel {
 		return count;
 	}
 	
+	@Command
+	@NotifyChange({"workspacePage"})
+	public void changePage(@BindingParam("page")String src){
+		logger.info("Setting workspace src to " + src);
+		setWorkspacePage(src);
+	}
+	
+	public String getWorkspacePage() {
+		return workspacePage;
+	}
+
+	public void setWorkspacePage(String workspacePage) {
+		this.workspacePage = workspacePage;
+	}
+
 	@GlobalCommand
 	public void logout() {
 //		AuthenticationService.logout();
